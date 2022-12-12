6d215cb7f3f27bc86b0793c12c2ae20f1dbbe8b2
arkuar <arttukuronen@gmail.com>
2022-12-12 19:34:38 +0200

    Optimize day 12

--- 12/day12.go
+++ 12/day12.go
@@ -9,20 +9,12 @@ import (
 
 func main() {
 	input := utils.ReadLines("input.txt")
-	grid, start, end, start2 := createGrid(input)
-	p1 := shortestPath(grid, start, end)
-
-	p2 := math.MaxInt
-	for _, s := range start2 {
-		res := shortestPath(grid, s, end)
-		if res != -1 && res < p2 {
-			p2 = res
-		}
-	}
+	grid, start, end := createGrid(input)
+	p1, p2 := shortestPaths(grid, start, end)
 	fmt.Printf("Part 1: %d\nPart 2: %d\n", p1, p2)
 }
 
-func createGrid(input []string) (grid [][]rune, start, end image.Point, start2 []image.Point) {
+func createGrid(input []string) (grid [][]rune, start, end image.Point) {
 	grid = make([][]rune, len(input))
 	for y, r := range input {
 		row := make([]rune, len(r))
@@ -33,8 +25,6 @@ func createGrid(input []string) (grid [][]rune, start, end image.Point, start2 [
 			} else if c == 'E' {
 				c = 'z'
 				end = image.Point{x, y}
-			} else if c == 'a' {
-				start2 = append(start2, image.Point{x, y})
 			}
 			row[x] = c
 		}
@@ -50,7 +40,7 @@ func adjacent(grid [][]rune, p image.Point) []image.Point {
 	for _, dir := range dirs {
 		nx, ny := p.X+dir.X, p.Y+dir.Y
 		if ny >= 0 && ny < len(grid) && nx >= 0 && nx < len(grid[0]) {
-			if grid[ny][nx]-grid[p.Y][p.X] <= 1 {
+			if grid[p.Y][p.X]-grid[ny][nx] <= 1 {
 				result = append(result, image.Point{nx, ny})
 			}
 		}
@@ -59,32 +49,33 @@ func adjacent(grid [][]rune, p image.Point) []image.Point {
 
 }
 
-func shortestPath(grid [][]rune, start, end image.Point) int {
-	q := []image.Point{start}
-	e := map[image.Point]image.Point{}
+type node struct {
+	point image.Point
+	steps int
+}
+
+func shortestPaths(grid [][]rune, start, end image.Point) (int, int) {
+	q := []node{{end, 0}}
+	e := map[image.Point]struct{}{}
+	p1, p2 := 0, math.MaxInt
 
 	for len(q) > 0 {
 		current := q[0]
 		q = q[1:]
-		if current.Eq(end) {
+		if grid[current.point.Y][current.point.X] == 'a' && current.steps < p2 {
+			p2 = current.steps
+		}
+		if current.point.Eq(start) {
+			p1 = current.steps
 			break
 		}
-		for _, a := range adjacent(grid, current) {
+		for _, a := range adjacent(grid, current.point) {
 			if _, ok := e[a]; !ok {
-				e[a] = current
-				q = append(q, a)
+				e[a] = struct{}{}
+				q = append(q, node{a, current.steps + 1})
 			}
 		}
 	}
 
-	if _, ok := e[end]; !ok {
-		return -1
-	}
-
-	var p1 int
-	for !end.Eq(start) {
-		p1++
-		end = e[end]
-	}
-	return p1
+	return p1, p2
 }
